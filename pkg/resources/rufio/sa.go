a1dca6dc19e4f76f493a36834b50e23b17462822
Moath Qasim <moad.qassem@gmail.com>
2022-11-04 01:16:04 +0100

    add rufio sa

---
+++ pkg/resources/rufio/sa.go
@@ -0,0 +1,33 @@
+package rufio
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	serviceAccountName = "rufio-controller-manager"
+)
+
+func CreateServiceAccount(ctx context.Context, client ctrlruntimeclient.Client, ns string) error {
+	sa := &corev1.ServiceAccount{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      serviceAccountName,
+			Namespace: ns,
+		},
+	}
+
+	if err := client.Create(ctx, sa); err != nil {
+		if kerrors.IsAlreadyExists(err) {
+			return nil
+		}
+
+		return err
+	}
+
+	return nil
+}
