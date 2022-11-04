97776c4515418234e03526cd4e4e3a77483a10a2
Moath Qasim <moad.qassem@gmail.com>
2022-11-04 16:59:11 +0100

    refactor service accounts

--- pkg/resources/boots/sa.go
+++ pkg/resources/boots/sa.go
@@ -3,7 +3,7 @@ package boots
 import (
 	"context"
 
-	rbacv1 "k8s.io/api/rbac/v1"
+	corev1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -14,9 +14,10 @@ const (
 )
 
 func CreateServiceAccount(ctx context.Context, client ctrlruntimeclient.Client, ns string) error {
-	sa := &rbacv1.ClusterRole{
+	sa := &corev1.ServiceAccount{
 		ObjectMeta: v1.ObjectMeta{
-			Name: serviceAccountName,
+			Name:      serviceAccountName,
+			Namespace: ns,
 		},
 	}
 
