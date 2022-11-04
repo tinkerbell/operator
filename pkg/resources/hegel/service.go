d9c45d124aaf7a4805a6cba376558e18cc5e64af
Moath Qasim <moad.qassem@gmail.com>
2022-11-04 01:16:34 +0100

    add hegel service

---
+++ pkg/resources/hegel/service.go
@@ -0,0 +1,46 @@
+package hegel
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func CreateService(ctx context.Context, client ctrlruntimeclient.Client, ns string) error {
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "hegel",
+			Namespace: ns,
+			Labels: map[string]string{
+				"app": "hegel",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			ClusterIP: "None",
+			Selector: map[string]string{
+				"app": "hegel",
+			},
+			Ports: []corev1.ServicePort{
+				{
+					Port:       50061,
+					TargetPort: intstr.IntOrString{StrVal: "hegel-http"},
+					Protocol:   corev1.ProtocolTCP,
+				},
+			},
+		},
+	}
+
+	if err := client.Create(ctx, service); err != nil {
+		if kerrors.IsAlreadyExists(err) {
+			return nil
+		}
+
+		return err
+	}
+
+	return nil
+}
