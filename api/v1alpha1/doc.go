1a1424381a73ee0ec54a4175aa0fd4a9b93bab0b
Moath Qasim <moad.qassem@gmail.com>
2023-08-16 13:30:58 +0200

    refactor tinkerbell crd
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>

--- api/v1alpha1/doc.go
+++ api/v1alpha1/doc.go
@@ -1,2 +1,6 @@
+// +k8s:deepcopy-gen:package
+// +k8s:openapi-gen=true
+
 // Package v1alpha1 contains API Schema definitions for the Tinkerbell operator v1alpha1 API group
+// +groupName=operator.tinkerbell.org
 package v1alpha1
--- api/v1alpha1/stack_type.go
+++ api/v1alpha1/stack_type.go
@@ -4,122 +4,128 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 // +kubebuilder:object:root=true
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// +kubebuilder:resource:path=tinkerbell,scope=Namespaced,categories=tinkerbell,singular=tinkerbell,shortName=tb
+// +kubebuilder:resource:path=stack,scope=Namespaced,categories=stack,singular=stack,shortName=tb
 // +kubebuilder:storageversion
 
-// Tinkerbell represents the tinkerbell stack that is being deployed in the kubernetes where the operator is deployed.
+// Stack represents the tinkerbell stack that is being deployed in the kubernetes where the operator is deployed.
 // Tinkerbell operator watches for different resources such as deployment, services, serviceAccounts, etc. One of those
-// CRs is Tinkerbell which the operator will install the tink-stack based on its specs. Once the CR is deleted,
+// CRs is Stack which the operator will install the tink-stack based on its specs. Once the CR is deleted,
 // the operator will delete all tinkerbell resources.
-type Tinkerbell struct {
+type Stack struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	// Spec describes the desired tinkerbell stack state.
-	Spec TinkerbellStackSpec `json:"spec,omitempty"`
+	Spec StackSpec `json:"spec"`
 }
 
-// TinkerbellStackSpec specifies details of an Tinkerbell setup.
-type TinkerbellStackSpec struct {
+// StackSpec specifies details of an Tinkerbell setup.
+type StackSpec struct {
 	// Version is the Tinkerbell CRD version.
 	Version string `json:"version"`
 
 	// DNSResolverIP is indicative of the resolver IP utilized for setting up the nginx server responsible for proxying
 	// to the Tinkerbell service and serving the Hook artifacts.
-	DNSResolverIP string `json:"dnsResolverIP"`
+	DNSResolverIP string `json:"dnsResolverIP,omitempty"`
 
 	// Registry is the registry to use for all images. If this field is set, all tink service deployment images
 	// will be prefixed with this value. For example if the value here was set to docker.io, then boots image will be
 	// docker.io/tinkerbell/boots.
-	Registry string `json:"registry"`
+	Registry string `json:"registry,omitempty"`
 
 	// ImagePullSecret the secret name containing the docker auth config which should exist in the same namespace where
 	// the operator is deployed(typically tinkerbell)
-	ImagePullSecret []string `json:"imagePullSecret"`
+	ImagePullSecret []string `json:"imagePullSecret,omitempty"`
 
 	// Services contains all Tinkerbell Stack services.
-	Services Services `json:"services"`
+	Services Services `json:"services,omitempty"`
 }
 
 // Services contains all Tinkerbell Stack services.
 type Services struct {
 	// Boots contains all the information and spec about boots.
-	Boots Boots `json:"boots"`
+	Boots Boots `json:"boots,omitempty"`
 
 	// Hegel contains all the information and spec about boots.
-	Hegel Hegel `json:"hegel"`
+	Hegel Hegel `json:"hegel,omitempty"`
 
 	// Rufio contains all the information and spec about rufio.
-	Rufio Rufio `json:"rufio"`
+	Rufio Rufio `json:"rufio,omitempty"`
 
 	// TinkServer contains all the information and spec about tink server.
-	TinkServer TinkServer `json:"tinkServer"`
+	TinkServer TinkServer `json:"tinkServer,omitempty"`
 
 	// TinkController contains all the information and spec about tink controller.
-	TinkController Image `json:"tinkController"`
+	TinkController TinkController `json:"tinkController,omitempty"`
 }
 
 // Boots specifies the deployment details of Tinkerbell service, Boots.
 type Boots struct {
 	// Image specifies the details of a tinkerbell services images
-	Image Image `json:"image"`
+	Image Image `json:"image,omitempty"`
 
 	// DHCPAddressListener set the ip and port to listen on for DHCP.
-	DHCPAddressListener string `json:"dhcpAddress"`
+	DHCPAddressListener string `json:"dhcpAddressListener,omitempty"`
 
 	// TrustedProxies comma separated allowed CIDRs subnets to be used as trusted proxies
-	TrustedProxies string `json:"trustedProxies"`
+	TrustedProxies []string `json:"trustedProxies,omitempty"`
 
 	// HTTPAddress is the address to listen on for the serving iPXE binaries and files via HTTP.
-	HTTPAddress string `json:"httpAddress"`
+	HTTPAddress string `json:"httpAddress,omitempty"`
 
 	// OSIEURL override the URL where OSIE/Hook images are located
-	OSIEURL string `json:"osieURL"`
+	OSIEURL string `json:"osieURL,omitempty"`
 
 	// PublicIP is the IP that netboot clients and/or DHCP relay's will use to reach Boots
-	PublicIP string `json:"publicIP"`
+	PublicIP string `json:"publicIP,omitempty"`
 
 	// PublicSyslogFQDN is the IP that syslog clients will use to send messages
-	PublicSyslogFQDN string `json:"publicSyslogFQDN"`
+	PublicSyslogFQDN string `json:"publicSyslogFQDN,omitempty"`
 
 	// SyslogAddress is the IP and port that syslog clients will use to send messages
-	SyslogAddress string `json:"syslogAddress"`
+	SyslogAddress string `json:"syslogAddress,omitempty"`
 
 	// TinkerbellGRPCAuthority IP and port to listen on for syslog messages.
-	TinkerbellGRPCAuthority string `json:"tinkerbellGRPCAuthority"`
+	TinkerbellGRPCAuthority string `json:"tinkerbellGRPCAuthority,omitempty"`
 
 	// TinkerbellTLS sets if the boots should run with TLS or not.
-	TinkerbellTLS bool `json:"tinkerbellTLS"`
+	TinkerbellTLS bool `json:"tinkerbellTLS,omitempty"`
 
 	// LogLevel sets the debug level for boots.
-	LogLevel string `json:"logLevel"`
+	LogLevel string `json:"logLevel,omitempty"`
 }
 
 // Hegel specifies the details of tinkerbell service hegel.
 type Hegel struct {
 	// Image specifies the details of a tinkerbell services images
-	Image Image `json:"image"`
+	Image Image `json:"image,omitempty"`
 
 	// TrustedProxies comma separated allowed CIDRs subnets to be used as trusted proxies
-	TrustedProxies string `json:"trustedProxies"`
+	TrustedProxies []string `json:"trustedProxies,omitempty"`
 }
 
 // Rufio specifies the details of tinkerbell service rufio.
 type Rufio struct {
 	// Image specifies the details of a tinkerbell services images
-	Image Image `json:"image"`
+	Image Image `json:"image,omitempty"`
 
 	// TrustedProxies comma separated allowed CIDRs subnets to be used as trusted proxies
-	TrustedProxies string `json:"trustedProxies"`
+	TrustedProxies []string `json:"trustedProxies,omitempty"`
 }
 
 // TinkServer specifies the details of tinkerbell service tink server.
 type TinkServer struct {
 	// Image specifies the details of a tinkerbell services images
-	Image Image `json:"image"`
+	Image Image `json:"image,omitempty"`
 
 	// TinkerbellTLS sets if the tink server should run with TLS or not.
-	TinkerbellTLS bool `json:"tinkerbellTLS"`
+	TinkerbellTLS bool `json:"tinkerbellTLS,omitempty"`
+}
+
+// TinkController specifies the details of tinkerbell service tink controller.
+type TinkController struct {
+	// Image specifies the details of a tinkerbell services images
+	Image Image `json:"image,omitempty"`
 }
 
 // Image specifies the details of a tinkerbell services images.
